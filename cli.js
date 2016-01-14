7c3a1d1d57e6db76d8a762557531ca24adaea0cb
Martin <martinvdpub@gmail.com>
2016-01-14 03:20:00 +0100

    First commit

---
+++ cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+'use strict';
+
+const prependFile = require('prepend-file');
+const argv = require('minimist')(process.argv.slice(2));
+
+const filename = argv._[0] || '';
+const data = argv._[1] || '';
+
+prependFile(filename, data, (err) => {
+	if (err) {
+		if (err.message) {
+			console.error(err.message);
+			process.exit(1);
+		} else {
+			throw err;
+		}
+	}
+});
---
+++ test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs');
+
+const assert = require('assert');
+const execa = require('execa');
+
+describe('Prepend to file', () => {
+	const testfile = '.temp1';
+
+	after(cb => fs.unlink(testfile, cb));
+
+	it('Should create an empty file and have content added', cb => {
+		execa('./cli.js', [testfile, 'Hello']).then(() => {
+			let fileData = fs.readFileSync(testfile).toString();
+			assert.strictEqual(fileData, 'Hello');
+			cb();
+		});
+	});
+
+	it('Should prepend data to a non empty file', cb => {
+		execa('./cli.js', [testfile, 'What']).then(() => {
+			let fileData = fs.readFileSync(testfile).toString();
+			assert.strictEqual(fileData, 'WhatHello');
+			cb();
+		});
+	});
+
+	it('Should accepts buffers', cb => {
+		let buf = new Buffer('abc', 'utf8');
+		execa('./cli.js', [testfile, buf]).then(() => {
+			let fileData = fs.readFileSync(testfile).toString();
+			assert.strictEqual(fileData, 'abcWhatHello');
+			cb();
+		});
+	});
+
+	it('Should accepts numbers', cb => {
+		let number = 220;
+		execa('./cli.js', [testfile, number]).then(() => {
+			let fileData = fs.readFileSync(testfile).toString();
+			assert.strictEqual(fileData, '220abcWhatHello');
+			cb();
+		});
+	});
+});
